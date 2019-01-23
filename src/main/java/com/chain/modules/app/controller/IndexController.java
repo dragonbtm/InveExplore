6a9e0ca9dd2071688c8e05b23effea82b8967457
zhang0135789 <zz634682577>
2019-01-23 11:45:59 +0800

    增加拉取transactions信息定时任务

--- src/main/java/com/chain/modules/app/controller/IndexController.java
+++ src/main/java/com/chain/modules/app/controller/IndexController.java
@@ -1,6 +1,9 @@
 package com.chain.modules.app.controller;
 
+import com.chain.common.utils.R;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @Modified By
  */
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/v1/index")
 @Api("1主页信息查询")
 public class IndexController {
+
+
+
+
+    @PostMapping("/search")
+    @ApiOperation("搜索栏目")
+    public R serch() {
+
+
+
+        return new R();
+    }
 }
--- src/main/java/com/chain/modules/app/controller/NodeController.java
+++ src/main/java/com/chain/modules/app/controller/NodeController.java
@@ -1,6 +1,11 @@
 package com.chain.modules.app.controller;
 
+import com.chain.common.utils.R;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +16,43 @@ import org.springframework.web.bind.annotation.RestController;
  * @Modified By
  */
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/v1/node")
 @Api("3节点信息")
 public class NodeController {
+
+    @GetMapping("/preview")
+    @ApiOperation("全网预览")
+    public R preview() {
+
+
+
+        return new R();
+    }
+
+
+    @PostMapping("/messageslist")
+    @ApiOperation("消息列表")
+    public R messagesList() {
+
+        return new R();
+    }
+
+
+    @PostMapping("/messagesinfo")
+    @ApiOperation("消息详细信息")
+    public R messagesinfo() {
+
+        return new R();
+    }
+
+
+
+    @PostMapping("/graphdatas")
+    @ApiOperation("图表数据")
+    public R graphdatas() {
+
+        return new R();
+    }
+
+
 }
